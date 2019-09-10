56fc54f91289eb9316999bd3ddce5d3feef896eb
fatmarekik2019 <fatma.rekik88@gmail.com>
2019-09-10 16:45:54 +0100

    verification username

--- src/main/java/projet/spring/FindMyTravel/services/ClientServiceImpl.java
+++ src/main/java/projet/spring/FindMyTravel/services/ClientServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.transaction.annotation.Transactional;
 import projet.spring.FindMyTravel.entities.Client;
 import projet.spring.FindMyTravel.entities.Role;
 import projet.spring.FindMyTravel.entities.Status;
+import projet.spring.FindMyTravel.entities.User;
 import projet.spring.FindMyTravel.repositories.ClientRepository;
 
 @Service("clientService")
@@ -25,11 +27,27 @@ public class ClientServiceImpl implements ClientService{
 	
 	@Transactional
 	@Override
-	public ResponseEntity<Client> addClient(Client c) {
+	public String addClient(Client c) {
 		c.setRole(Role.client);
 		c.setStatus(Status.activated);
 		em.persist(c);
-		return ResponseEntity.ok().build();
+		return "ok";
+	}
+	
+	@Transactional
+	@Override
+	public Boolean verifUserName(String username) {
+		
+		TypedQuery<User> query = (TypedQuery<User>) em.createQuery("SELECT u FROM User u WHERE u.userName = :username" ,User.class);
+		List<User> u=query.setParameter("username", username).getResultList();
+		
+		if(u.isEmpty() == true) {
+			
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 	
 	@Transactional
